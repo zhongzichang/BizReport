f1a7b94a633ee6c3e6ecd06cf7db61dda42eb545
Bingo <zhong.zc@gaiasys.cn>
2018-01-20 17:08:10 +0800

    first commit

---
+++ components/DetailsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DetailsScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Details Screen</Text>
+  </View>
+);
+
+export default DetailsScreen;
---
+++ components/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button
+      onPress={() => navigation.navigate('Details')}
+      title="Go to details"
+    />
+  </View>
+);
+
+export default HomeScreen;
+
