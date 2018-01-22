fbf7ad72d9962b0e648f6a3d2ec7d91d96bff4aa
zhong zichang <zhongzichang@gmail.com>
2018-01-22 14:04:11 +0800

    完成财务页面初稿

--- components/FinanceScreen.js
+++ components/FinanceScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button, FlatList } from 'react-native';
+import { VictoryBar } from "victory-native";
 
 class FinanceScreen extends React.Component {
 
@@ -9,11 +10,112 @@ class FinanceScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center' }}>
-        <FlatList
-          data={[{key: 'a', value: 1}, {key: 'b', value: 2}]}
-          renderItem={({item}) => <Text>{item.key} - {item.value}</Text>}
-        />
+      <View>
+
+        <View style={{flexDirection: 'row'}}>
+          <Text>收入</Text>
+          <View>
+            <View style={{flexDirection: 'row'}}>
+              <Text>零售</Text>
+              <View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text>现金</Text>
+                  <Text>2098</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text>刷卡</Text>
+                  <Text>2098</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text>支付宝</Text>
+                  <Text>2098</Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text>微信</Text>
+                  <Text>2098</Text>
+                </View>
+              </View>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text>合计</Text>
+              <Text>3421</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <Text>支出</Text>
+          <View>
+            <View style={{flexDirection: 'row'}}>
+              <Text>供货商</Text>
+              <View>
+                <View>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text>货款</Text>
+                    <Text>20000</Text>
+                  </View>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text>其他</Text>
+                    <Text></Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text>联营商</Text>
+              <View>
+                <View>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text>分成结算</Text>
+                    <Text>1368</Text>
+                  </View>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text>其他</Text>
+                    <Text>70</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text>合计</Text>
+              <Text>21438</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <Text>净收入</Text>
+          <Text>-18017</Text>
+        </View>
+
+        <Text>本周预算（万）</Text>
+
+        <View style={{flexDirection: 'row'}}>
+          <View>
+            <Text>零售收入</Text>
+            <Text>3200</Text>
+          </View>
+          <View>
+            <Text>经销商回款</Text>
+            <Text>1280</Text>
+          </View>
+          <View>
+            <Text>供应商付款</Text>
+            <Text>9600</Text>
+          </View>
+          <View>
+            <Text>联营分成</Text>
+            <Text>1280</Text>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <Text>赤字</Text>
+          <Text>-6400</Text>
+        </View>
+
+        <VictoryBar />
+
       </View>
     );
   }
