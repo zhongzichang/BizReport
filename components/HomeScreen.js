c4bf216c4c497b7a42f83f05fd083ce42e6dc511
zhong zichang <zhongzichang@gmail.com>
2018-01-23 11:11:57 +0800

    加入 react-native-material-kit 包，增加 登录组件

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
+import LoginModal from './LoginModal';
 
-const HomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen</Text>
-    <Button
-      onPress={() => navigation.navigate('Performance')}
-      title="Go to Performance"
-    />
-  </View>
-);
+class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: '首页'
+  };
+
+
+  constructor() {
+    super();
+    this.state = {
+      logined: false,
+      data: []
+    };
+  }
+
+
+  render() {
+
+    const { navigate } = this.props.navigation;
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          onPress={() => navigate('Performance')}
+          title="Go to Performance"
+        />
+        <LoginModal />
+      </View>
+
+    );
+  }
+}
 
 export default HomeScreen;
---
+++ components/LoginModal.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Text, View, Button, Modal, StyleSheet } from 'react-native';
+import { MKTextField, MKColor, MKButton } from 'react-native-material-kit';
+
+export default class LoginModal extends Component {
+  state = {
+    modalVisible: true,
+  };
+
+  openModal() {
+    this.setState({modalVisible:true});
+  }
+
+  closeModal() {
+    this.setState({modalVisible:false});
+  }
+
+  render() {
+    const LoginButton = MKButton.coloredButton()
+      .withText('登录')
+      .withStyle(styles.loginButton)
+      .withOnPress(() => {
+        this.closeModal();
+      })
+      .build();
+
+    return (
+        <View style={styles.container}>
+          <Modal
+              visible={this.state.modalVisible}
+              animationType={'slide'}
+              onRequestClose={() => this.closeModal()}
+          >
+            <View style={styles.modalContainer}>
+              <View style={styles.innerContainer}>
+                <MKTextField
+                  tintColor={MKColor.Lime}
+                  textInputStyle={{color: MKColor.Orange}}
+                  placeholder="用户名"
+                  style={styles.textfield}
+                />
+                <MKTextField
+                  tintColor={MKColor.Lime}
+                  textInputStyle={{color: MKColor.Orange}}
+                  placeholder="密码"
+                  style={styles.textfield}
+                />
+
+                <LoginButton />
+
+              </View>
+            </View>
+          </Modal>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  innerContainer: {
+    alignItems: 'center',
+  },
+  textfield: {
+    height: 28,  // have to do it on iOS
+    width: 200,
+    marginTop: 20,
+  },
+  textfieldWithFloatingLabel: {
+    height: 48,  // have to do it on iOS
+    marginTop: 20,
+  },
+  loginButton: {
+    marginTop: 20,
+  }
+});
